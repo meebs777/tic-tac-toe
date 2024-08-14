efa1f828cdc80f28a954970d007b53a53fca39dd
Mibo <meebsawosika@gmail.com>
2024-08-13 23:14:39 -0600

    Created game control function with working methods

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const gameBoard = (function() {
     const columns = 3;
     const board = [];
 
-
-    for(i = 0; i < rows; i++) {
-        board[i] = [];
-        for(j = 0; j < columns; j++){
-            board[i][j] = "X";
+    const initializeBoard = () => {
+        for(i = 0; i < rows; i++) {
+            board[i] = [];
+            for(j = 0; j < columns; j++){
+                board[i][j] = "";
+            }
         }
     }
+
+    initializeBoard();
+    
     
     const getBoard = () => board;
 
@@ -23,7 +27,101 @@ const gameBoard = (function() {
         return board[row][column];
     }
 
-    return {displayBoard,placeToken,getBoard,getToken}
+    return {displayBoard,placeToken,getBoard,getToken,initializeBoard}
 })();
 
+function  gameController (playerOneName = "Player One", playerTwoName = "Player Two") {
+    players = [
+        {
+            name: playerOneName, 
+            token: "X"},
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ]
 
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        gameBoard.displayBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const gameWin = () => {
+        const activeToken = getActivePlayer().token;
+
+        //Checking row wins
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(gameBoard.getToken(i,j)=== activeToken && j != 2) {continue}
+                else if(gameBoard.getToken(i,j)=== activeToken && j === 2) {return (`${getActivePlayer().name}'s win`);}
+                else {break;}
+            }
+        }
+        //Checking column wins
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(gameBoard.getToken(j,i)=== activeToken && j != 2) {continue}
+                else if(gameBoard.getToken(j,i)=== activeToken && j === 2) {return (`${getActivePlayer().name}'s win`);}
+                else {break;}
+            }
+        }
+
+        //Checking diagonal win
+        if(gameBoard.getToken(0,0) === activeToken){
+            if(gameBoard.getToken(1,1) === activeToken) {
+                if(gameBoard.getToken(2,2) === activeToken) {
+                    return (`${getActivePlayer().name}'s win`);
+                }
+            }
+        }
+
+        //Checking diagonal win
+        if(gameBoard.getToken(2,0) === activeToken){
+            if(gameBoard.getToken(1,1) === activeToken) {
+                if(gameBoard.getToken(0,2) === activeToken) {
+                    return (`${getActivePlayer().name}'s win`);
+                }
+            }
+        }
+
+    }
+
+    const gameWinreset = (gameCheck) => {
+        if(gameCheck === `${getActivePlayer().name}'s win`) {
+            console.log(`${getActivePlayer().name}'s win`);
+            if(getActivePlayer().name === players[0].name) {
+                switchPlayerTurn();
+            }
+            gameBoard.initializeBoard();
+        }
+    }
+
+    const playRound = (row,column) => {
+
+
+        gameBoard.placeToken(row,column,getActivePlayer().token)
+
+
+        const gameCheck = gameWin();
+        gameWinreset(gameCheck);
+
+        
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {playRound,getActivePlayer};
+};
+
+const game = gameController("P1","P2");
